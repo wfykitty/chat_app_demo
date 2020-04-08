e53002e1e3dd671f11d5b5091e8badccbb42b899
Zakariya Jasat <zakariya.jasat@gmail.com>
2020-04-08 16:05:15 -0400

    FS3 Project

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
-const express = require('express')
+const express = require("express");
 const mongoose = require("mongoose");
-const moment = require('moment');
-const historyModel = require("./models/chathistory");  
-const eventlogModel = require("./models/eventlog");  
-
+const moment = require("moment");
+const historyModel = require("./models/chathistory");
+const eventlogModel = require("./models/eventlog");
 
 const app = express();
-// mongoose.connect("mongodb://feiya:Feiya126@cluster0-shard-00-00-h9bay.azure.mongodb.net:27017,cluster0-shard-00-01-h9bay.azure.mongodb.net:27017,cluster0-shard-00-02-h9bay.azure.mongodb.net:27017/admin_data?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority");
 
 const connectionString =
-"mongodb://feiya:Feiya126@cluster0-shard-00-00-h9bay.azure.mongodb.net:27017,cluster0-shard-00-01-h9bay.azure.mongodb.net:27017,cluster0-shard-00-02-h9bay.azure.mongodb.net:27017/admin_data?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+    "mongodb://feiya:Feiya126@cluster0-shard-00-00-h9bay.azure.mongodb.net:27017,cluster0-shard-00-01-h9bay.azure.mongodb.net:27017,cluster0-shard-00-02-h9bay.azure.mongodb.net:27017/admin_data?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true})
-  .then(
-    () => {
-      console.log("Mongoose connected successfully ");
-    },
-    error => {
-      console.log("Mongoose could not connected to database : " + error);
-    }
-  );
+    .connect(connectionString, {
+        useNewUrlParser: true
+    })
+    .then(
+        () => {
+            console.log("Mongoose connected successfully ");
+        },
+        error => {
+            console.log("Mongoose could not connected to database : " + error);
+        }
+    );
 
 //set the template engine ejs
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 //middlewares
-app.use(express.static('public'))
-
+app.use(express.static("public"));
 
 //routes
-app.get('/', (req, res) => {
-	res.render('index')
-})
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
 //Listen on port 5000
-server = app.listen(5000, ()=> console.log("Server connected."))
+server = app.listen(5000, () => console.log("Server connected."));
 
 //socket.io instantiation
-const io = require("socket.io")(server)
-
+const io = require("socket.io")(server);
 
 //listen on every connection
-io.on('connection', (socket) => {
-	console.log('New user connected')
+io.on("connection", socket => {
+    console.log("New user connected");
 
-  
     // listen for disconnect
     socket.connection = "Connected";
 
@@ -61,99 +57,165 @@ io.on('connection', (socket) => {
         socket.connection = data.disconnect;
         savedLog({
             error: "User disconnected",
-            message: `${socket.username} is now Disconnected`
+            message: `${socket.username} now left the room and disconnected`
         });
     });
 
-//log function
-    async function savedLog(data){
+    //log function
+    async function savedLog(data) {
         const log = new eventlogModel({
-        errorType: data.error,
-        errorMessage: data.message,
-        date: moment().format(),
-        })
-        const result =  await log.save();
-        console.log('*********LOG TABLE*********')
+            errorType: data.error,
+            errorMessage: data.message,
+            date: moment().format()
+        });
+        const result = await log.save();
+        console.log("*********LOG STORED IN DATABASE*********");
         console.log(result);
     }
 
-	//default username
-	socket.username = "Anonymous"
-
+    //default username
+    socket.username = "Anonymous";
     //listen on change_username
-    socket.on('change_username', (data) => {
-        if (data.username == ''){
-            savedLog({error:'Error', message: `Please enter your username.`, })
-      }
-        let oldname = socket.username
-        socket.username = data.username
-        savedLog({error:'Change Username', message: `${oldname} is now ${data.username}`, })
-    })
+    socket.on("change_username", data => {
+        if (data.username == "") {
+            savedLog({
+                error: "Error",
+                message: `Please enter your username.`
+            });
+        }
+        let oldname = socket.username;
+        socket.username = data.username;
+        savedLog({
+            error: "Change Username",
+            message: `${oldname} is now ${socket.username}`
+        });
+    });
 
+    // listen to room 1
+    socket.on("room1Bth", () => {
+        console.log("Now joined room 1!");
+        //join the room
+        socket.join("room1Bth");
+        savedLog({
+            error: "New Room",
+            message: `${socket.username} joined Room 1`
+        });
+        //send chat log ******
+        historyModel
+            .find({ room: "room1" }, function(err, docs) {
+                if (!err) {
+                    console.log(docs);
+                    let data = docs.map(x => {
+                        let _ = { user: x.sender, message: x.message };
+                        return _;
+                    });
+                    io.sockets.emit("ChatLog1", data);
+                } else {
+                    throw err;
+                }
+            })
+            .sort("-date")
+            .limit(10);
+        // io.sockets.emit("ChatLog1", {
+        //     user: room1ChatLog.sender,
+        //     message: room1ChatLog.message
+        // });
+        //broadcast to room
+        
+            io.to("room1Bth")
+            .emit("new_message", "User has joined the room 1 chat");
+    });
 
-    //////////////////////////
-  // listen on rooms
-  ////////////////////////
-socket.on('room1Bth', () => {
-  console.log("Now joined room1!");
-      //join the room 
-  socket.join("room1Bth");
-      //broadcast to entire room
-  io.sockets.to("room1Bth").emit(
-        "room1",
-        "user has joined to room1 chat");});
+    // listen to room 2
+    socket.on("room2Bth", () => {
+        console.log("Now joined room 2!");
+        //join the room
+        socket.join("room2Bth");
+        savedLog({
+            error: "New Room",
+            message: `${socket.username} joined Room 2`
+        });
+        //send chat log ******
+        historyModel
+            .find({ room: "room2" }, function(err, docs) {
+                if (!err) {
+                    console.log(docs);
+                    let data = docs.map(x => {
+                        let _ = { user: x.sender, message: x.message };
+                        return _;
+                    });
+                    console.log("******Chat of room2****")
+                    console.log(data)
+                    io.to('room2Bth').emit("ChatLog2", data);
+                } else {
+                    throw err;
+                }
+            })
+            .sort("-date")
+            .limit(10);
+        // io.sockets.emit("ChatLog2", {
+        //     user: room1ChatLog.sender,
+        //     message: room1ChatLog.message
+        // });
+        //broadcast to room
+        
+            io.to("room2Bth")
+            .emit("new_message", "User has joined the room 2 chat");
 
+            
+    });
 
-socket.on('room2Bth', () => {
-   console.log("Now joined room2!");
-              //join the room 
-   socket.join("room2Bth");
-              //broadcast to entire room
-              alert('Now joined room2!')
-    io.sockets.to("room2Bth").emit(
-         "room1",
-         "user has joined to room2 chat");}
-    
-         );
+    //listen event log & history log
+    socket.on("eventLog", () => {
+        eventlogModel.find({}, function(err, docs) {
+            if (!err) {
+                console.log(docs);
+                socket.emit('eventHistory', docs)
+            } else {
+                throw err;
+            }
+        }).limit(10)
+        .sort("-date")
+    });
 
-
-
-
-//////////////////
-//Print Event log
-socket.on('eventLog', () => {
-  console.log("EVENT LOG");
-});
-//   function savedLog(data){
-//     const log = new eventlogModel({
-//     errorType: data.error,
-//     errorMessage: data.message,
-    // date: moment().format(),
-//   })}
-
+    socket.on("chatlog", () => {
+        historyModel.find({}, function(err, docs) {
+            if (!err) {
+                console.log(docs);
+                socket.emit('chatHistory', docs)
+            } else {
+                throw err;
+            }
+        }
+        
+        ).limit(10)
+        .sort("-date")
+    });
 
     //listen on new_message
-    socket.on('new_message', (data) => {
+    socket.on("new_message", data => {
         //broadcast the new message
-        async function savedMessage(){
+        console.log(data.room)
+        async function savedMessage() {
             const chats = new historyModel({
                 sender: socket.username,
+                room: data.room,
                 date: moment().format(),
-                // room: "room1",
                 message: data.message
-            })
-            const result =  await chats.save();
+            });
+            const result = await chats.save();
             console.log(result);
         }
         savedMessage();
-        io.sockets.emit('new_message', {message : data.message, username : socket.username});
-    })
-      
-    socket.on('typing', (data) => {
-    	socket.broadcast.emit('typing', {username : socket.username})
-    })
+        io.sockets.emit("new_message", {
+            message: data.message,
+            username: socket.username
+        });
+    });
+
+    socket.on("typing", data => {
+        socket.broadcast.emit("typing", { username: socket.username });
+    });
 
     // io.sockets.emit('new_message', {message : data.message, username : socket.username});
-})
-
-
+});
--- public/chat.js
+++ public/chat.js
@@ -1,87 +1,175 @@
+$(function() {
+    //make connection
+    var socket = io.connect("http://localhost:5000");
 
-$(function(){
-   	//make connection
-	var socket = io.connect('http://localhost:5000')
+    //buttons and inputs
+    var message = $("#message");
+    var username = $("#username");
+    var send_message = $("#send_message");
+    var connected = $("#connected");
+    var disconnected = $("#disconnected");
+    var send_username = $("#send_username");
+    var chatroom = $("#chatroom");
+    var feedback = $("#feedback");
+    var room1Bth = $("#room1");
+    var room2Bth = $("#room2");
+    var eventLogButton = $("#event_log");
+    var chatlogButton = $("#chatlog");
 
-	//buttons and inputs
-	var message = $("#message")
-	var username = $("#username")
-	var send_message = $("#send_message")
-	var connected = $("#connected")
-	var disconnected = $("#disconnected")
-	var send_username = $("#send_username")
-	var chatroom = $("#chatroom")
-	var feedback = $("#feedback")
-	var room1Bth = $("#room1")
-	var room2Bth = $("#room2")
-	const eventLogButton = $("#event_log");
+    var currentRoom="room1";
+    //room 1 channel
+    room1Bth.click(() => {
+        //this clears the chat
+        chatroom.empty();
+        currentRoom = 'room1'
+        socket.emit("room1Bth", { message: "Joined room 1 sucessfully" });
+        console.log("User joined room 1");
+    });
 
+    socket.on("ChatLog1", data => {
+        console.log(data);
+            feedback.html("");
+            message.val("");
+        for (let i=0; i<data.length; i++) {
+            // console.log(data[i])
+            chatroom.append(
+                "<p class='message'>" + data[i].user + ": " + data[i].message + "</p>"
+            );
+        }
+    });
 
-	//room channel 
-	room1Bth.click(() => {
-		socket.emit('room1Bth', {message : "joined the room1 sucessfully"});
-		console.log("user joined the room1");
-		;
-	});
-	room2Bth.click(() => {
-		socket.emit('room2Bth', {message : "joined the room2 sucessfully"});
-		console.log("user joined the room2");
-	});
+    //room 2 channel
+    room2Bth.click(() => {
+        chatroom.empty();
+        currentRoom = 'room2'
+        socket.emit("room2Bth", { message: "Joined room 2 sucessfully" });
+        console.log("User joined room 2");
+    });
 
+    socket.on("ChatLog2", data => {
+        console.log(data);
+            feedback.html("");
+            message.val("");
+        for (let i=0; i<data.length; i++) {
+            // console.log(data[i])
+            chatroom.append(
+                "<p class='message'>" + data[i].user + ": " + data[i].message + "</p>"
+            );
+        }
+    });
 
+    
+    //Emit message
+    send_message.click(function() {
+        console.log("Clicked");
+        socket.emit("new_message", { message: message.val(), room: currentRoom });
+    });
+    
+    //Emit connection and disconnection
+    connected.click(function() {
+        socket.emit("connected", { message: "connected to the chat room" });
+    });
+    disconnected.click(function() {
+        socket.emit("disconnected", { message: "disconnected" });
+    });
 
-///////////
-////event log 
+    //Listen on new_message
+    socket.on("new_message", data => {
+        feedback.html("");
+        message.val("");
+        chatroom.append(
+            "<p class='message'>" + data.username + ": " + data.message + "</p>"
+        );
+    });
+    
+    //Listen on connection and disconnection
+    socket.on("connected", data => {
+        feedback.html("");
+        message.val("");
+        chatroom.append(
+            "<p class='message'>" + data.username + "has connected to the room"
+        );
+    });
+    socket.on("disconnected", data => {
+        feedback.html("");
+        message.val("");
+        chatroom.append(
+            "<p class='message'>" +
+                data.username +
+                "has disconnected from the room"
+        );
+    });
+    
+    //Emit a username
+    send_username.click(function() {
+        socket.emit("change_username", { username: username.val() });
+    });
+    
+    //Emit typing
+    message.bind("keypress", () => {
+        socket.emit("typing");
+    });
+    
+    //Listen on typing
+    socket.on("typing", data => {
+        feedback.html(
+            "<p><i>" + data.username + " is typing a message..." + "</i></p>"
+        );
+    });
 
-	eventLogButton.click(() => {
-		socket.emit('eventLog');
-		console.log("event hapening");
-	})
-	
+    //event & history log
+    eventLogButton.click(() => {
+        console.log('event button clicked')
+        document.getElementById('historyTab').style.display="none";
+        document.getElementById('eventTab').style.display="block";
+        socket.emit("eventLog");
+    });
 
-	//Emit message
-	send_message.click(function(){
-		console.log('Clicked');
-		socket.emit('new_message', {message : message.val(),
-	 })
-	})
-	//Emit connection and disconnection
-	connected.click(function(){
-		socket.emit('connected', {message : "connected sucessafully"})
-	})
-	disconnected.click(function(){
-		socket.emit('disconnected', {message : "disconnected"})
-	})
- 
-	//Listen on new_message
-	socket.on("new_message", (data) => {
-		feedback.html('');
-		message.val('');
-		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
-	})
-	//Listen on connection and disconnection
-	socket.on("connected", (data) => {
-		feedback.html('');
-		message.val('');
-		chatroom.append("<p class='message'>" + data.username + "has connected to the room" )
-	})
-	socket.on("disconnected", (data) => {
-		feedback.html('');
-		message.val('');
-		chatroom.append("<p class='message'>" + data.username + "has disconnected to the room" )
-	})
-	//Emit a username
-	send_username.click(function(){
-		socket.emit('change_username', {username : username.val()})
-	})
-	//Emit typing
-	message.bind("keypress", () => {
-		socket.emit('typing')
-	})
-	//Listen on typing
-	socket.on('typing', (data) => {
-		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
-	})
+    chatlogButton.click(function() {
+        console.log('chat butoon clicked')
+        document.getElementById('eventTab').style.display="none";
+        document.getElementById('historyTab').style.display="block";
+        socket.emit("chatlog");
+    });
+
+    socket.on("chatHistory", docs => {
+        let tbody = $('#historyChat')
+     
+            tbody.html('')
+            for(let i=0; i<docs.length; i++){
+                tbody.append(
+                    `
+                    <tr style="text-align:center">
+                        <td>${docs[i].sender}</td>
+                        <td>${docs[i].message}</td>
+                        <td>${docs[i].room}</td>
+                        <td>${docs[i].date}</td>
+                    </tr>
+                    `
+                )
+            }
+
+        
+        console.log(docs)
+    })
+
+    socket.on("eventHistory", docs => {
+        let tbody = $('#eventChat')
+        tbody.html('')
+        for(let i=0; i<docs.length; i++){
+            tbody.append(
+                `
+                <tr style="text-align:center">
+                    <td>${docs[i].errorType}</td>
+                    <td>${docs[i].errorMessage}</td>
+                    <td>${docs[i].date}</td>
+                </tr>
+                `
+            )
+        }
+        console.log(docs)
+    })
 });
 
- 
+
+
